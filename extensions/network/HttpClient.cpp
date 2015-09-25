29497e7aa39dd5b15e64a206d95cec604fd2861a
Cem Uzunlar <cem.uzunlar@infosfer.com>
2015-09-25 22:04:54 +0300

    openssl multithreading updates.

--- extensions/network/HttpClient.cpp
+++ extensions/network/HttpClient.cpp
@@ -227,6 +227,60 @@ static void* networkThread(void *data)
     return 0;
 }
 
+#define MUTEX_TYPE       pthread_mutex_t
+#define MUTEX_SETUP(x)   pthread_mutex_init(&(x), NULL)
+#define MUTEX_CLEANUP(x) pthread_mutex_destroy(&(x))
+#define MUTEX_LOCK(x)    pthread_mutex_lock(&(x))
+#define MUTEX_UNLOCK(x)  pthread_mutex_unlock(&(x))
+#define THREAD_ID        pthread_self(  )
+
+/* This array will store all of the mutexes available to OpenSSL. */
+static MUTEX_TYPE *mutex_buf= NULL;
+
+static void locking_function(int mode, int n, const char * file, int line)
+{
+	if (mode & CRYPTO_LOCK)
+		MUTEX_LOCK(mutex_buf[n]);
+	else
+		MUTEX_UNLOCK(mutex_buf[n]);
+}
+
+static void id_function(CRYPTO_THREADID* threadid)
+{
+	CRYPTO_THREADID_set_numeric(threadid, (unsigned long)pthread_self());
+}
+
+int thread_setup(void)
+{
+	int i;
+ 
+	mutex_buf = (MUTEX_TYPE*)malloc(CRYPTO_num_locks(  ) * sizeof(MUTEX_TYPE));
+	if (!mutex_buf)
+		return 0;
+	for (i = 0;  i < CRYPTO_num_locks();  i++)
+		MUTEX_SETUP(mutex_buf[i]);
+	CRYPTO_THREADID_set_callback(id_function);
+	CRYPTO_set_locking_callback(locking_function);
+	return 1;
+}
+
+int thread_cleanup(void)
+{
+	int i;
+ 
+	if (!mutex_buf)
+		return 0;
+	CRYPTO_set_id_callback(NULL);
+	CRYPTO_set_locking_callback(NULL);
+	for (i = 0;  i < CRYPTO_num_locks(  );  i++)
+		MUTEX_CLEANUP(mutex_buf[i]);
+	free(mutex_buf);
+	mutex_buf = NULL;
+	return 1;
+}
+
+/////////////////
+
 //Configure curl's timeout property
 static bool configureCURL(CURL *handle)
 {
@@ -425,6 +479,9 @@ CCHttpClient::CCHttpClient()
 : _timeoutForConnect(30)
 , _timeoutForRead(60)
 {
+
+	thread_setup();
+	
     _curlProxyIp = NULL;
     _curlProxyPort = -1;
     
@@ -442,6 +499,8 @@ CCHttpClient::~CCHttpClient()
     }
     
     s_pHttpClient = NULL;
+	
+	thread_cleanup();
 }
 
 //Lazy create semaphore & mutex & thread
